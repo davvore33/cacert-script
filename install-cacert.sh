41d9b6386ab735e7a70ee27001cc42be4ed2a1d2
matteo <davvore33@gmail.com>
2015-02-21 14:16:17 +0100

    modified:   install-cacert.sh

--- install-cacert.sh
+++ install-cacert.sh
@@ -1,6 +1,10 @@
 primof=`cat nome1.txt`
 secondf=`cat nome2.txt`
 
+echo attenzione se adb sta gia girando, va chiuso, ora lo farò partire con i permessi di root
+
+adb root
+
 echo rimonto la partizione /system
 adb remount
 
