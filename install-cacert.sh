9ff35031f9bb0588f32a83563940fbbc34a90b82
matteo <davvore33@gmail.com>
2015-02-22 12:06:52 +0100

    modified:   install-cacert.sh

--- install-cacert.sh
+++ install-cacert.sh
@@ -1,14 +1,28 @@
 primof=`cat nome1.txt`
 secondf=`cat nome2.txt`
 
-echo attenzione se adb sta gia girando, va chiuso, ora lo farò partire con i permessi di root
 
+#termino ogni adb server preesistente, necessario a causa di incompatibilità per il comando remount
+adb kill-server
+#faccio partire adb con i permessi di root, necessario sempre per remount
 adb root
 
-echo rimonto la partizione /system
+state=`adb get-state`
+
+if [ $state != "device" ]
+then
+	echo non hai un cellulare collegato
+	exit 1
+fi
+
+#necessario rimontare /system con i permessi di scrittura, se la procedura non va a buon fine
+#adb shell; su; mount -o remount, rw /system
 adb remount
 
 echo push dei file
+
+#copio i file .0 dei due certificati su /sdcard, dopodiche ne cambio i permessi e infine li sposto nella
+#cartella di sistema
 
 chmod 644 $primof
 chmod 644 $secondf
@@ -16,4 +30,5 @@ chmod 644 $secondf
 adb push $primof /system/etc/security/cacerts/
 adb push $secondf /system/etc/security/cacerts/
 
-echo ricorda di riavviare il cellulare \[adb reboot \]
+echo ricorda di riavviare il cellulare \[adb reboot \], al termine della procedura dovresti poter\
+vedere \"CaCert\" fra i certificati utenti validi \[Impostazioni -\> Sicurezza\]
